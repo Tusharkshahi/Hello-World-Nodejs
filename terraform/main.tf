81dd9218878d99006a32957dc2fb903902ffdc71
Tushar Kumar Shahi <103762351+Tusharkshahi@users.noreply.github.com>
2024-06-10 12:11:32 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,88 +2,68 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_iam_role" "ecs_task_execution" {
+resource "aws_ecs_cluster" "helloWorld" {
+  name = "helloWorld-cluster"
+}
+
+# IAM Role for ECS Task Execution
+resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
+        Action = "sts:AssumeRole",
         Effect = "Allow",
         Principal = {
           Service = "ecs-tasks.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
+        }
       }
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
-  role       = aws_iam_role.ecs_task_execution.name
+  role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-}
-
-resource "aws_security_group" "ecs" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_ecs_cluster" "main" {
-  name = "hello-world-cluster"
-}
-
-resource "aws_ecs_task_definition" "app" {
-  family                   = "hello-world-task"
+# Define the ECS task definition
+resource "aws_ecs_task_definition" "helloWorld" {
+  family                   = "hello-world"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([{
-    name      = "hello-world"
-    image     = "851725266600.dkr.ecr.ap-south-1.amazonaws.com/hello-world-nodejs:latest"
-    essential = true
-    portMappings = [{
-      containerPort = 80
-      hostPort      = 80
-    }]
-  }])
+  container_definitions = jsonencode([
+    {
+      name      = "hello-world"
+      image     = "851725266600.dkr.ecr.ap-south-1.amazonaws.com/hello-world-nodejs:latest" # Update this with your ECR repository URI
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 3000
+          hostPort      = 3000
+        }
+      ]
+    }
+  ])
 }
 
-resource "aws_ecs_service" "app" {
-  name            = "hello-world-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.app.arn
+# Define the ECS service
+resource "aws_ecs_service" "helloWorld" {
+  name            = "helloWorld-service"
+  cluster         = aws_ecs_cluster.helloWorld.id
+  task_definition = aws_ecs_task_definition.helloWorld.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
-    subnets         = [subnet-005fc6c4dc01a48d3]
-    security_groups = [sg-091a92188a1d830bc]
+    subnets         = ["subnet-005fc6c4dc01a48d3"]
+    security_groups = ["sg-091a92188a1d830bc"]
   }
 }
